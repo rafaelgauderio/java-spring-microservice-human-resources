2f489c67fd9d556a86ba0eb8d5466432feb6801c
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 16:48:16 -0300

    Add ribbon dependency

--- human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
+++ human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
@@ -1,8 +1,5 @@
 package com.example.humanresourcepayment.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
