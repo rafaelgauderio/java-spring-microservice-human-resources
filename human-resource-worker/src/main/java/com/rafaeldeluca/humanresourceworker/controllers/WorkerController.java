cfe90b70309a7b334ed710abf91019eb407dd84d
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 17:17:26 -0300

    load balincing with ribbon

--- human-resource-worker/src/main/java/com/rafaeldeluca/humanresourceworker/controllers/WorkerController.java
+++ human-resource-worker/src/main/java/com/rafaeldeluca/humanresourceworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package com.rafaeldeluca.humanresourceworker.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import com.rafaeldeluca.humanresourceworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
+	@Autowired
+	private Environment environment;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -27,6 +35,9 @@ public class WorkerController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findWorkerById(@PathVariable Long id) {
+		
+		logger.info("LOCAL SERVER PORT: " + environment.getProperty("local.server.port"));
+		
 		Worker workerOptional = workerRepository.findById(id).get();
 		return ResponseEntity.ok(workerOptional);
 	}
