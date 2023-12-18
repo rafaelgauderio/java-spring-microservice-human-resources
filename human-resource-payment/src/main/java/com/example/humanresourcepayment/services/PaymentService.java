7a728de525b539a5cd4b799414b92f1b7bec2bd8
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 15:58:25 -0300

    Connection between projects with RestTemplate

--- human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
+++ human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
@@ -1,15 +1,34 @@
 package com.example.humanresourcepayment.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.humanresourcepayment.entities.Payment;
+import com.example.humanresourcepayment.entities.Worker;
 
 @Service
 public class PaymentService {
-	
-	public Payment getPatmentPerWorker (Long id, Integer days) {
-		// pagamento mocado
-		return new Payment("Rafael de Luca", 300.0, days);
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public Payment getPatmentPerWorker(Long workerId, Integer days) {
+
+		// dicionário de parametros com chave e valor da interface Map
+		Map<String, String> uriParameters = new HashMap<String, String>();
+		uriParameters.put("id", String.valueOf(workerId));
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriParameters);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
 	}
 
 }
