ebe124b34ae351067215b57072f7b60ff95b1519
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 16:23:37 -0300

    Connection between projects with openfeign

--- human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
+++ human-resource-payment/src/main/java/com/example/humanresourcepayment/services/PaymentService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.humanresourcepayment.WorkerFeignClient;
 import com.example.humanresourcepayment.entities.Payment;
 import com.example.humanresourcepayment.entities.Worker;
 
@@ -20,15 +21,17 @@ public class PaymentService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
 	public Payment getPatmentPerWorker(Long workerId, Integer days) {
 
 		// dicionário de parametros com chave e valor da interface Map
-		Map<String, String> uriParameters = new HashMap<String, String>();
-		uriParameters.put("id", String.valueOf(workerId));
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriParameters);
+		// Map<String, String> uriParameters = new HashMap<String, String>();
+		// uriParameters.put("id", String.valueOf(workerId));
+		// Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",
+		// Worker.class, uriParameters);
+		Worker worker = workerFeignClient.findWorkerById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-
 	}
-
 }
