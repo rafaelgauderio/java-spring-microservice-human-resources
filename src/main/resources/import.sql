ac3fc2594e42ba03cd6048be46d9f9e7666e36a5
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 13:12:48 -0300

    application.propeties and initial seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Rafael', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Claudia', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Lara', 350.0);
