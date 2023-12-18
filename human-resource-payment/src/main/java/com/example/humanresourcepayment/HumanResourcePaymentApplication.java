1e7d6957ce36e8a14f959ff98a15de7800950a58
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 14:40:36 -0300

    create payment project

---
+++ human-resource-payment/src/main/java/com/example/humanresourcepayment/HumanResourcePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanresourcepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcePaymentApplication.class, args);
+	}
+
+}
---
+++ human-resource-payment/src/test/java/com/example/humanresourcepayment/HumanResourcePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanresourcepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
