27d877e63cb3d4e8850e7266c74024f6ad75dc49
Rafael de Luca <deluca1712@gmail.com>
2023-12-18 12:54:00 -0300

    created project

---
+++ src/test/java/com/rafaeldeluca/humanresourceworker/HumanResourceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.humanresourceworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
