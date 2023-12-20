091b72360433febec21db449fed581781d0d7132
Rafael de Luca <deluca1712@gmail.com>
2023-12-20 16:01:14 -0300

    configuring project worker as eureka client

--- human-resource-worker/src/main/java/com/rafaeldeluca/humanresourceworker/HumanResourceWorkerApplication.java
+++ human-resource-worker/src/main/java/com/rafaeldeluca/humanresourceworker/HumanResourceWorkerApplication.java
@@ -2,7 +2,9 @@ package com.rafaeldeluca.humanresourceworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HumanResourceWorkerApplication {
 
