36731107f2898688e85db5c7faa99dc815128d9e
Rafael de Luca <deluca1712@gmail.com>
2023-12-20 11:29:56 -0300

    Created eureka-server project

---
+++ human-resource-eureka-server/src/main/java/com/example/humanresourceeurekaserver/HumanResourceEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.humanresourceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//@EnableEurekaServer
+@SpringBootApplication
+public class HumanResourceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ human-resource-eureka-server/src/test/java/com/example/humanresourceeurekaserver/HumanResourceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanresourceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
