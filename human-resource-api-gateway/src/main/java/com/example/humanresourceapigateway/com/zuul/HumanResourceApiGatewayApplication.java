ce64d6ec436ea9885a786c1ef5fd51ce156517f0
Rafael de Luca <deluca1712@gmail.com>
2023-12-21 15:16:34 -0300

    Configuring api-gateway-zuul

--- human-resource-api-gateway/src/main/java/com/example/humanresourceapigateway/com/zuul/HumanResourceApiGatewayApplication.java
+++ human-resource-api-gateway/src/main/java/com/example/humanresourceapigateway/com/zuul/HumanResourceApiGatewayApplication.java
@@ -2,9 +2,13 @@ package com.example.humanresourceapigateway.com.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 // gatewall zuul vai rotear as requisiçõe a partir dos nomes dos microsserviços
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HumanResourceApiGatewayApplication {
 
