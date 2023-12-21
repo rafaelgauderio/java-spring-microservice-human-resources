24e8fb4cb9d9bb5290df6ee5011f88246344e64d
Rafael de Luca <deluca1712@gmail.com>
2023-12-21 16:40:31 -0300

    create project hr-config-server

---
+++ human-resource-config-server/src/main/java/com/example/humanhrconfigserver/HumanResourceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanhrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceConfigServerApplication.class, args);
+	}
+
+}
---
+++ human-resource-config-server/src/test/java/com/example/humanhrconfigserver/HumanResourceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanhrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
