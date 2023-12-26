5f3cabbed1a607538678b7ada24d550d76b1f7f6
Rafael de Luca <deluca1712@gmail.com>
2023-12-26 15:16:03 -0300

    created hr-user project

---
+++ human-resource-hr-user/src/main/java/com/example/humanhruserserver/HumanResourceHrUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanhruserserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceHrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceHrUserApplication.class, args);
+	}
+
+}
---
+++ human-resource-hr-user/src/test/java/com/example/humanhruserserver/HumanResourceHrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanhruserserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceHrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
