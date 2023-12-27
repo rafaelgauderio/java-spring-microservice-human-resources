9f8fdfeb630440eec15f9684b5130288f24ac2e4
Rafael de Luca <deluca1712@gmail.com>
2023-12-27 13:44:55 -0300

    create project hr-oauth

---
+++ human-resource-hr-oauth/src/main/java/com/example/humanhruserauth/HumanResourceHrOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanhruserauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceHrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceHrOauthApplication.class, args);
+	}
+
+}
---
+++ human-resource-hr-oauth/src/test/java/com/example/humanhruserauth/HumanResourceHrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanhruserauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceHrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
